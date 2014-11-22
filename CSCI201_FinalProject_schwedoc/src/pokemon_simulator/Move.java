a73de51f2a581399c26bc86ae6bc877a31b01966
Brian Schwedock <schwedoc@usc.edu>
2014-11-22 14:48:18 -0800

    Wrote Move class

--- CSCI201_FinalProject_schwedoc/src/pokemon_simulator/Move.java
+++ CSCI201_FinalProject_schwedoc/src/pokemon_simulator/Move.java
@@ -1,5 +1,39 @@
 package pokemon_simulator;
 
 public class Move {
-
+	private String name;
+	//Design doc said "type" is int, but in Pokemon class is String
+	private String type;
+	private int attack;
+	private int specialAttack;
+	private int accuracy;
+	
+	public Move (String name, String type, int attack,
+			int specialAttack, int accuracy) {
+		this.name = name;
+		this.type = type;
+		this.attack = attack;
+		this.specialAttack = specialAttack;
+		this.accuracy = accuracy;
+	}
+	
+	public String getName () {
+		return name;
+	}
+	
+	public String getType () {
+		return type;
+	}
+	
+	public int getAttack () {
+		return attack;
+	}
+	
+	public int getSpecialAttack () {
+		return specialAttack;
+	}
+	
+	public int getAccuracy () {
+		return accuracy;
+	}
 }
