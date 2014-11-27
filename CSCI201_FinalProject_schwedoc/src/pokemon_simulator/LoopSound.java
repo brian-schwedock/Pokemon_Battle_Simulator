53468b4a9f6f068626e610485c03d53f739ab039
ryanc_000 <ryanc_000@Work>
2014-11-26 17:28:48 -0800

    Sound for the game

---
+++ CSCI201_FinalProject_schwedoc/src/pokemon_simulator/LoopSound.java
@@ -0,0 +1,39 @@
+package pokemon_simulator;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.*;
+import javax.sound.sampled.*;
+
+public class LoopSound implements Runnable{
+	public void run(){
+		try{
+	        File file = new File(
+	            "Sound\\champion-battle.mid");
+	        Clip clip;
+			try {
+				clip = AudioSystem.getClip();
+				 // getAudioInputtream() also accepts a File or InputStream
+		        AudioInputStream ais=null;
+				try {
+					ais = AudioSystem.
+					    getAudioInputStream( file );
+				} catch (UnsupportedAudioFileException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        clip.open(ais);
+		        clip.loop(Clip.LOOP_CONTINUOUSLY);
+		       
+			} catch (LineUnavailableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	       return;
+    	}
+    	catch (IOException e) {
+    		    throw new RuntimeException(e);
+    	}
+	}
+	
+}
