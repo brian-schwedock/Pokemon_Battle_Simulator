a61d7ada3fd17038325c67e2981072d628a4df06
ryanc_000 <ryanc_000@Work>
2014-11-26 19:38:11 -0800

    fixed textwrapping

--- CSCI201_FinalProject_schwedoc/src/pokemon_simulator/GameApplication.java
+++ CSCI201_FinalProject_schwedoc/src/pokemon_simulator/GameApplication.java
@@ -31,8 +31,19 @@ import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.UIManager;
+import javax.swing.text.AbstractDocument;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.BoxView;
+import javax.swing.text.ComponentView;
+import javax.swing.text.Element;
+import javax.swing.text.IconView;
+import javax.swing.text.LabelView;
+import javax.swing.text.ParagraphView;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
+import javax.swing.text.StyledEditorKit;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
 
 public class GameApplication extends JFrame {
 
@@ -124,6 +135,7 @@ public class GameApplication extends JFrame {
 		chatBoxPanel.setLayout(new BoxLayout (chatBoxPanel, BoxLayout.Y_AXIS));
 
 		chatTextPane = new JTextPane ();
+		chatTextPane.setEditorKit(new WrapEditorKit());
 		doc = chatTextPane.getStyledDocument();
 		chatTextPane.setPreferredSize(new Dimension (400, 572));
 		chatTextPane.setEnabled(false);
@@ -724,12 +736,59 @@ public class GameApplication extends JFrame {
 			GameApplication ga = new GameApplication (stc, outToServer);
 			ClientThread ct = new ClientThread (inFromServer, ga);
 			ct.start();
-			Thread t=new Thread(clip);
-			t.start();
+			//Thread t=new Thread(clip);
+			//t.start();
 			//startGame.close(); 
 		} catch (Exception e){ 
 			System.out.println("Please run the server first"); 
 		} 	
 
+	}
+	class WrapEditorKit extends StyledEditorKit {
+	    ViewFactory defaultFactory=new WrapColumnFactory();
+	    public ViewFactory getViewFactory() {
+	        return defaultFactory;
+	    }
+	
+	}
+
+	class WrapColumnFactory implements ViewFactory {
+	    public View create(Element elem) {
+	        String kind = elem.getName();
+	        if (kind != null) {
+	            if (kind.equals(AbstractDocument.ContentElementName)) {
+	                return new WrapLabelView(elem);
+	            } else if (kind.equals(AbstractDocument.ParagraphElementName)) {
+	                return new ParagraphView(elem);
+	            } else if (kind.equals(AbstractDocument.SectionElementName)) {
+	                return new BoxView(elem, View.Y_AXIS);
+	            } else if (kind.equals(StyleConstants.ComponentElementName)) {
+	                return new ComponentView(elem);
+	            } else if (kind.equals(StyleConstants.IconElementName)) {
+	                return new IconView(elem);
+	            }
+	        }
+	
+	        // default to text display
+	        return new LabelView(elem);
+	    }
+	}
+	
+	class WrapLabelView extends LabelView {
+	    public WrapLabelView(Element elem) {
+	        super(elem);
+	    }
+	
+	    public float getMinimumSpan(int axis) {
+	        switch (axis) {
+	            case View.X_AXIS:
+	                return 0;
+	            case View.Y_AXIS:
+	                return super.getMinimumSpan(axis);
+	            default:
+	                throw new IllegalArgumentException("Invalid axis: " + axis);
+	        }
+	    }
+	
 	}
 }
